8bd4d4d905a8c15eb746db052aacba2208e39d8c
Cru Scanlan <cruscanlan@gmail.com>
2022-07-16 09:14:22 +1000

    Action Test

---
+++ index.js
@@ -0,0 +1,15 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { promises: fs } = require('fs');
+
+const main = async () => {
+    const repo = core.getInput('repo');
+    console.log('ScratchLink Version Control Running For Repo: ' + repo);
+    
+    if(repo === 'editConfig') {
+        const scratchLinkConfig = await fs.readFile('./config.json', 'utf8');
+        console.log(scratchLinkConfig)
+    }
+}
+
+main().catch(err => core.setFailed(err.message));
