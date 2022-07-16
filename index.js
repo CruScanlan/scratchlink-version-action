42cd6ea239dae6cda9a40c57641c2cddaa351ef8
Cru Scanlan <cruscanlan@gmail.com>
2022-07-16 10:49:46 +1000

    Added Version

--- index.js
+++ index.js
@@ -8,13 +8,22 @@ const workspace = process.env.GITHUB_WORKSPACE;
 const main = async () => {
     const repo = core.getInput('repo');
     console.log('ScratchLink Version Control Running For Repo: ' + repo);
-
     const dir = path.resolve(workspace);
 
-    console.log(await fs.readdir(dir));
     if(repo === 'editConfig') {
-        const scratchLinkConfig = await fs.readFile(path.join(dir, 'config.json'), 'utf8');
-        console.log(scratchLinkConfig);
+        const scratchLinkConfigString = await fs.readFile(path.join(dir, 'config.json'), 'utf8');
+        const scratchLinkConfig = JSON.parse(scratchLinkConfigString);
+
+        scratchLinkConfig.versionDetails = {
+            commitUrl: `${github.context.repo.repoUrl}/commit/${github.context.sha}`,
+            commitCount: github.context.payload.pull_request.commits.length,
+            time: (new Date()).toTimeString()
+        }
+
+        await fs.writeFile(path.join(dir, 'config.json'), JSON.stringify(scratchLinkConfig, null, 2));
+
+        console.log('ScratchLink Version Control: Updated config.json');
+        console.log('ScratchLink Version Control: ' + JSON.stringify(scratchLinkConfig.versionDetails));
     }
 }
 
